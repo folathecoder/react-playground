d96a19c77884bdcea22f0a28d6b2f02ed80a3284
folathecoder <officialakinloyefolarin@gmail.com>
2021-08-27 16:15:36 +0100

    Compartmentalization

---
+++ my-app/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <div>
+            <footer>
+                <h1>This is the FOOTER SECTION</h1>
+            </footer>
+        </div>
+    )
+}
+
+export default Footer;
---
+++ my-app/src/components/Main.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Main() {
+    return (
+        <div>
+            <main>
+                <h1>This is the MAIN SECTION</h1>
+            </main>
+        </div>
+    )
+}
+
+export default Main;
